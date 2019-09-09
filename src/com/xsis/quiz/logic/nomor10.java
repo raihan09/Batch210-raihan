6f218d629667bda987742beb6c22ed9504452f60
raihan09 <99afiq@gmail.com>
2019-09-09 16:56:42 +0700

    quiz

---
+++ src/com/xsis/quiz/logic/nomor10.java
@@ -0,0 +1,18 @@
+package com.xsis.quiz.logic;
+import java.util.Scanner;
+public class nomor10 {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        char q;
+        System.out.println("Please enter values, Q to quit");
+        int a = 0;
+        String y [] = {keyboard.nextLine()};
+        for (int i = 0; i< y.length ; i++) {
+            System.out.println();
+            System.out.println(y[i]);
+
+                }
+            }
+        }
+
+
